b95573548033f073b5fb7b9806af49356197af3f
kalai_625 <kalaiyarasan61097@gmail.com>
2022-10-28 17:47:17 +0530

    C5

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,10 +2,12 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
- // await page.goto('https://playwright.dev/');
-  await page.goto('https://www.annulartechnologies.com/');
-  
 
+  await page.goto('https://www.annulartechnologies.com/');
+  const ref =await page.title();  
+  console.log(ref);
+  
+  // await page.goto('https://playwright.dev/');
   // // Expect a title "to contain" a substring.
   // await expect(page).toHaveTitle(/Playwright/);
 
